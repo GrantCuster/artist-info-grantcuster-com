6595a59f4bc2d401ebe667d735ff9179745a0fb7
Grant Custer <grantcuster@gmail.com>
2025-11-05 10:45:33 -0500

    design tweaks

--- frontend/src/NowPlaying.tsx
+++ frontend/src/NowPlaying.tsx
@@ -62,14 +62,14 @@ export function NowPlaying() {
   }, [nowPlaying]);
 
   return (
-    <div className="flex gap-5 overflow-hidden">
+    <div className="flex gap-8 overflow-hidden">
       <div className="max-w-[400px] aspect-square w-full shrink-0">
         <img src={nowPlaying?.item?.album.images[0]?.url} alt="Album Art" />
       </div>
-      <div className="grow text-lg flex flex-col justify-center pr-4">
-        <div className="mb-2">
-          <em>{nowPlaying?.item?.name}</em> from{" "}
-          <em>{nowPlaying?.item?.album.name}</em>
+      <div className="grow text-2xl flex flex-col justify-center pr-4">
+        <div className="mb-[1lh]">
+          <div>{nowPlaying?.item?.name}</div>
+          <div className="text-neutral-400">{nowPlaying?.item?.album.name}</div>
         </div>
         <div>
           {artistCache[nowPlaying?.item?.artists[0]?.name || ""]?.status ===
